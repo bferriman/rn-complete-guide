b3946183a6c534fa50aad9a750604a39f1861213
Ben F <56423163+bferriman@users.noreply.github.com>
2021-03-08 16:49:11 -0700

    converted in line styling to StyleSheet

--- App.js
+++ App.js
@@ -1,24 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{padding: 30}}>
-      <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
-        <TextInput
-          placeholder="Course Goal"
-          style={{width: "80%", borderColor: "black", borderWidth: 1, padding: 10}}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
-      <View>
-
-      </View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+  screen: {
+    padding: 30
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10
+  }
 });

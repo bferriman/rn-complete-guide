a446a336611f85d70ed018c5c521b8ce73e88002
Ben F <56423163+bferriman@users.noreply.github.com>
2021-03-08 14:13:32 -0700

    created text input field and button with inline styling

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <View>
-
+    <View style={{padding: 30}}>
+      <View style={{flexDirection: "row", justifyContent: "space-between", alignItems: "center"}}>
+        <TextInput
+          placeholder="Course Goal"
+          style={{width: "80%", borderColor: "black", borderWidth: 1, padding: 10}}
+        />
+        <Button title="ADD" />
       </View>
       <View>
 
